684ce61ff7c8564f0fa568fb04b10af4dacf3f0d
osa1 <omeragacan@gmail.com>
2014-01-26 13:43:51 +0200

    implement some TODOs left before

--- Lc.v
+++ Lc.v
@@ -1,5 +1,7 @@
 
 Require Export SfLib.
+Require Import Coq.Lists.ListSet.
+Require Import List. Open Scope list_scope.
 
 
 Inductive tm :=
@@ -22,7 +24,31 @@ Tactic Notation "tm_cases" tactic(first) ident(c) :=
 
 
 Inductive value : nat -> tm -> Prop :=
-| vvvv : forall n, value 10 (tnat n). (* TODO: implement this *)
+(* nats are values in all stages *)
+| vnat : forall n l, value l (tnat n)
+
+(* stage 0 values *)
+| vabs_0 : forall v t, value 0 (tabs v t)
+| vfix_0 : forall i1 i2 t, value 0 (tfix i1 i2 t)
+| vbox_0 : forall v, value 1 v -> value 0 (tbox v)
+
+(* stage n > 0 values *)
+| vvar_n : forall n i,
+    n > 0 -> value n (tvar i)
+| vabs_n : forall n v i,
+    n > 0 -> value n v -> value n (tabs i v)
+| vapp_n : forall n t1 t2,
+    n > 0 -> value n t1 -> value n t2 -> value n (tapp t1 t2)
+| vfix_n : forall f x v n,
+    n > 0 -> value n v -> value n (tfix f x v)
+| vbox_n : forall n v,
+    n > 0 -> value n v -> value (n+1) (tbox v)
+| unbox_n : forall n v,
+    n > 1 -> value (n-1) v -> value n (tunbox v)
+| run_n : forall n v,
+    n > 0 -> value n v -> value n (trun v).
+
+Hint Constructors value.
 
 
 Reserved Notation "'[' x ':=' s ']' t" (at level 20).
@@ -49,8 +75,29 @@ Fixpoint subst (x : id) (s : tm) (t : tm) : tm :=
 where "'[' x ':=' s ']' t" := (subst x s t).
 
 
-Inductive closed : nat -> tm -> Prop :=
-| c_nat : forall n, closed 0 (tnat n). (* TODO: implement this *)
+Fixpoint fvs (n : nat) (t : tm) : set id :=
+  match n with
+  | 0 =>
+      match t with
+      | tnat _ => nil
+      | tvar i => i :: nil
+      | tabs i t' => set_remove eq_id_dec i (fvs n t')
+      | tapp t1 t2 => set_union eq_id_dec (fvs n t1) (fvs n t2)
+      | tfix i1 i2 t' => set_remove eq_id_dec i1 (set_remove eq_id_dec i2 (fvs n t'))
+      | tbox t' => fvs (n + 1) t'
+      | tunbox t' => fvs n t' (* I think this case should not happen *)
+      | trun t' => fvs n t'
+      end
+  | n =>
+      match t with
+      | tunbox t' => fvs (n-1) t'
+      | _ => nil
+      end
+  end.
+
+
+Fixpoint closed (n : nat) (t : tm) : bool :=
+  beq_nat (length (fvs n t)) 0.
 
 
 Inductive step : tm -> nat -> tm -> Prop :=
@@ -75,7 +122,7 @@ Inductive step : tm -> nat -> tm -> Prop :=
     step (trun e) n (trun e')
 | s_run : forall v,
     value 1 v ->
-    closed 0 v ->
+    closed 0 v = true ->
     step (trun (tbox v)) 0 v
 | s_unb1 : forall e n e',
     step e n e' ->
