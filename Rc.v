d1f37297711a6f54a843d179ca89497a44ecb848
osa1 <omeragacan@gmail.com>
2014-03-17 11:37:39 +0200

    trying to implement substitutions

--- Rc.v
+++ Rc.v
@@ -5,6 +5,10 @@ Require Import List. Open Scope list_scope.
 Require Import FunctionalExtensionality.
 
 
+Inductive var_kind :=
+| holevar : var_kind
+| recvar : var_kind
+| ordvar : var_kind.
 
 
 Inductive tm :=
@@ -23,6 +27,7 @@ with rec :=
 | rextend1 : rec -> id -> id -> rec
 | rextend2 : rec -> id -> tm -> rec.
 
+
 Inductive value : tm -> Prop :=
 | vnat : forall n, value (tnat n)
 | vabs : forall i1 t, value (tabs i1 t)
@@ -35,3 +40,62 @@ with recordval : rec -> Prop :=
     value t ->
     recordval r ->
     recordval (rextend2 r i t).
+
+
+Inductive substx :=
+| substx_var : id -> tm -> substx
+| substx_rec1 : id -> rec -> substx. (* I think because of some bug in Coq I can't use name
+                                        substx_rec even though it's not defined in elsewhere *)
+
+
+Inductive subst : substx -> tm -> tm -> Prop :=
+| snat : forall x n, subst x (tnat n) (tnat n)
+| svar : forall i t, subst (substx_var i t) (tvar i) t
+| svar1 : forall a b r, subst (substx_rec1 a r) (tvar b) (tvar b)
+| svar2 : forall a b t, a <> b -> subst (substx_var a t) (tvar b) (tvar b)
+| sabs : forall x i t b b',
+    x <> i ->
+    subst (substx_var x t) b b' ->
+    subst (substx_var x t) (tabs i b) (tabs i b')
+| sabs' : forall x i r b b',
+    subst (substx_rec1 x r) b b' ->
+    subst (substx_rec1 x r) (tabs i b) (tabs i b')
+| sabs1 : forall x t b,
+    subst (substx_var x t) (tabs x b) (tabs x b)
+| sapp : forall x t1 t2 t1' t2',
+    subst x t1 t1' ->
+    subst x t2 t2' ->
+    subst x (tapp t1 t2) (tapp t1' t2')
+| srec : forall x r r',
+    subst_rec x r r' ->
+    subst x (trec r) (trec r')
+| srec_select : forall x i r r',
+    subst_rec x r r' ->
+    subst x (trec_select r i) (trec_select r' i)
+| slet : forall i x t t1 t2 t1' t2',
+    i <> x ->
+    subst (substx_var x t) t1 t1' ->
+    subst (substx_var x t) t2 t2' ->
+    subst (substx_var x t) (tlet i t1 t2) (tlet i t1' t2')
+| slet' : forall x r i t1 t2 t1' t2',
+    subst (substx_rec1 x r) t1 t1' ->
+    subst (substx_rec1 x r) t2 t2' ->
+    subst (substx_rec1 x r) (tlet i t1 t2) (tlet i t1' t2')
+| slet1 : forall i t t1 t2 t1',
+    subst (substx_var i t) t1 t1' ->
+    subst (substx_var i t) (tlet i t1 t2) (tlet i t1' t2)
+
+with subst_rec : substx -> rec -> rec -> Prop :=
+| srec_empty : forall x, subst_rec x rempty rempty
+| srec_var : forall i r,
+    subst_rec (substx_rec1 i r) (rvar i) r
+| srec_var' : forall a b t,
+    subst_rec (substx_var a t) (rvar b) (rvar b)
+| srec_var'' : forall a b r,
+    a <> b ->
+    subst_rec (substx_rec1 a r) (rvar b) (rvar b)
+| srec_extend : forall r i t x t',
+    subst x t t' ->
+    subst_rec x (rextend2 r i t) (rextend2 r i t').
+
+
